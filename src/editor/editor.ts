fc15577adb9f6af596f2eec0d0c099be39dcc322
Diego Pérez <diego_prz@outlook.com>
2019-09-20 17:48:26 +0200

    Add core files to run the editor

---
+++ src/editor/editor.ts
@@ -0,0 +1,53 @@
+import Cursor from './cursor';
+import Line from './line';
+import Lines from './lines';
+
+const cursor = new Cursor(document.getElementById('cursor'));
+const editor = document.getElementById('editor');
+const lines = new Lines(new Line());
+let timeSinceLastChange = new Date();
+
+window.setInterval(() => {
+    const currentTime = new Date();
+    if (currentTime.getTime() - timeSinceLastChange.getTime() > 500) {
+        cursor.updateVisibility();
+    }
+}, 500);
+
+document.addEventListener('keydown', logKey);
+
+function logKey(e: KeyboardEvent): void {
+    e.preventDefault();
+    cursor.setVisible();
+
+    if (e.key.length === 1) {
+        lines.addText(e.key);
+    }
+    if (e.keyCode === 13) {
+        lines.add(new Line());
+    }
+    if (e.keyCode === 8) {
+        lines.removeChar();
+    }
+    if (e.keyCode === 37) {
+        lines.selectPreviousChar();
+    }
+    if (e.keyCode === 39) {
+        lines.selectNextChar();
+    }
+    if (e.keyCode === 38) {
+        lines.selectPreviousLine();
+    }
+    if (e.keyCode === 40) {
+        lines.selectNextLine();
+    }
+
+    timeSinceLastChange = new Date();
+    cursor.setPosition(lines.selectedLine().position, lines.selectedLineIndex());
+    renderLines();
+}
+
+function renderLines(): void {
+    editor.innerHTML = '';
+    lines.render(editor);
+}
